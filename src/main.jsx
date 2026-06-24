69d75aa59c95cc22a384813c755c336a6614178a
Huy <huynguyen@Huy-3.local>
2026-06-24 18:13:29 +0700

    feat: add vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </>
 )
